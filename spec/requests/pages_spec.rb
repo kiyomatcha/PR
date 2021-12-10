5ffecd521be96b53fbb85f81ef8c53e87ff774b6
KIYOMATCHA <athlete.kyogo@gmail.com>
2021-12-10 15:07:27 +0900

    Gem bootstrap-sass追加, pages_spec修正

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,40 +4,40 @@ RSpec.describe "pages", type: :request do
   describe "トップページ" do
     it "正常なレスポンスを返すこと" do
       get root_path
-      expect(response).to be_success
-      expect(response).to have_http_status "200"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "サービス紹介ページ" do
     it "正常なレスポンスを返すこと" do
       get about_path
-      expect(response).to be_success
-      expect(response).to have_http_status "200"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "よくある質問ページ" do
     it "正常なレスポンスを返すこと" do
       get help_path
-      expect(response).to be_success
-      expect(response).to have_http_status "200"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "利用規約ページ" do
     it "正常なレスポンスを返すこと" do
       get use_of_terms_path
-      expect(response).to be_success
-      expect(response).to have_http_status "200"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "よくある質問ページ" do
     it "正常なレスポンスを返すこと" do
       get privacy_path
-      expect(response).to be_success
-      expect(response).to have_http_status "200"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 end
