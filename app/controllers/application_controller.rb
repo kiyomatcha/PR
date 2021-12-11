90215d72f34a9ef6be1e2daae2f5b1c41a8fa08e
KIYOMATCHA <athlete.kyogo@gmail.com>
2021-12-11 10:29:09 +0900

    showページ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def after_sign_in_path_for(resource)
+    show_path
   end
+
+    private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
